b0778b1df96bd75164ba291b38981eff7da107ba
Tom McKenzie <tom@chillidonut.com>
2023-07-13 10:43:00 +1000

    wip cleanup

--- src/platform.ts
+++ src/platform.ts
@@ -1,4 +1,4 @@
-import {
+import type {
   Characteristic,
   CharacteristicValue,
   DynamicPlatformPlugin,
@@ -7,7 +7,9 @@ import {
   PlatformAccessory,
   PlatformConfig,
   Service,
+  HAP,
 } from "homebridge";
+import { APIEvent } from "homebridge";
 import WebSocket from "ws";
 import { UPSTREAM_API } from "./settings";
 import { HAPNodeJSClient } from "hap-node-client";
@@ -30,7 +32,7 @@ function createMessage(
 export class HomebridgeAI implements DynamicPlatformPlugin {
   private socket: WebSocket | undefined;
   private reconnectAttempts = 0;
-  private hapClient: ReturnType<HAPNodeJSClient>;
+  private hap: ReturnType<HAPNodeJSClient>;
 
   private hapReady = false;
   private socketReady = false;
@@ -40,44 +42,46 @@ export class HomebridgeAI implements DynamicPlatformPlugin {
     public readonly config: PlatformConfig,
     public readonly api: HomebridgeAPI,
   ) {
-    this.log.info("AI: Preparing for launch...");
+    this.log.info("Preparing for launch...");
 
     // Connect to Homebridge in insecure mode
-    this.hapClient = new HAPNodeJSClient({ debug: false, keepalive: false });
-
-    this.hapClient.on("Ready", () => {
-      this.log.info("Connected to HAP");
-      this.hapReady = true;
-      this.fetchAllDevicesAndCharacteristics();
-
-      // this.hapClient.HAPaccessories((response) => {
-      //   this.log.info(
-      //     'AI: BOOT Got accessories',
-      //     typeof response, response?.length,
-      //   );
-      // });
+    this.hap = new HAPNodeJSClient({
+      debug: false,
+      pin: undefined,
     });
 
-    this.api.on("didFinishLaunching", () => {
-      this.log.info("AI: Launching...");
+    this.hap.on("Ready", () => {
+      this.log.info("HAP Ready");
+      this.hapReady = true;
+      this.fetchAllDevicesAndCharacteristics();
+    });
+
+    this.api.on(APIEvent.DID_FINISH_LAUNCHING, () => {
+      this.log.info("Launching...");
       this.connectSocket();
       // this.fetchAllDevicesAndCharacteristics();
-      this.hapClient.on("event", this.handleCharacteristicChange.bind(this));
+      this.hap.on("event", this.handleCharacteristicChange.bind(this));
+    });
+    this.api.on(APIEvent.SHUTDOWN, () => {
+      this.log.info("Shutting down");
+      this.connectSocket();
+      // this.fetchAllDevicesAndCharacteristics();
+      this.hap.on("event", this.handleCharacteristicChange.bind(this));
     });
   }
 
   async fetchAllDevicesAndCharacteristics() {
     if (!this.hapReady || !this.socketReady) {
-      this.log.warn("Connections not yet ready");
+      this.log.warn(
+        "Connections not yet ready",
+        this.hapReady,
+        this.socketReady,
+      );
       return;
     }
 
-    this.hapClient.HAPaccessories((response) => {
-      this.log.info(
-        "AI: BOOT Got accessories",
-        typeof response,
-        response?.length,
-      );
+    this.hap.HAPaccessories((response) => {
+      this.log.info("BOOT Got accessories", typeof response, response?.length);
       this.log.debug(response);
       this.log.debug(response?.[0]?.accessories?.accessories);
 
@@ -93,7 +97,7 @@ export class HomebridgeAI implements DynamicPlatformPlugin {
     // const result = await getAccessories();
     // console.log({ result });
     // this.hapClient.HAPaccessories((err: unknown, response: any[]) => {
-    //   this.log.info('AI: Got accessories', response);
+    //   this.log.info('Got accessories', response);
     //   for (const device of response) {
     //     for (const service of device.services) {
     //       for (const characteristic of service.characteristics) {
@@ -108,7 +112,7 @@ export class HomebridgeAI implements DynamicPlatformPlugin {
   }
 
   handleCharacteristicChange(data: any) {
-    this.log.debug(`AI: Characteristic changed: ${JSON.stringify(data)}`);
+    this.log.debug(`Characteristic changed: ${JSON.stringify(data)}`);
     this.sendMessage({
       type: "deviceStatusChange",
       data: createMessage(data.accessory, data.service, data.characteristic),
@@ -121,7 +125,7 @@ export class HomebridgeAI implements DynamicPlatformPlugin {
     });
 
     this.socket.on("open", () => {
-      this.log.info("Connected to upstream");
+      this.log.info("Server connection ready");
       this.reconnectAttempts = 0; // reset reconnect attempts
       this.socketReady = true;
       this.fetchAllDevicesAndCharacteristics();
@@ -132,7 +136,7 @@ export class HomebridgeAI implements DynamicPlatformPlugin {
 
       if (event === "deviceStatus") {
         // Update the device properties when a message is received from the upstream API
-        await this.hapClient.HAPcontrol(
+        await this.hap.HAPcontrol(
           "::1",
           data.id,
           data.type,
@@ -175,7 +179,7 @@ export class HomebridgeAI implements DynamicPlatformPlugin {
 
   configureAccessory(_accessory: PlatformAccessory): void {
     this.log.debug(
-      `AI: Got configure accessory call for ${_accessory.displayName} ${_accessory.UUID}`,
+      `Got configure accessory call for ${_accessory.displayName} ${_accessory.UUID}`,
     );
     // This plugin doesn't own any accessories so we don't need to implement this
   }
